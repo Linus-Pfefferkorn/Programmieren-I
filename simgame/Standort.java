d9248e1994b3317677df4419a1267b9b9ad6970a
Linus-Pfefferkorn <104551105+Linus-Pfefferkorn@users.noreply.github.com>
2022-05-13 14:41:56 +0200

    Add files via upload

---
+++ simgame/Standort.java
@@ -0,0 +1,13 @@
+package demo001.simgame;
+
+public abstract class Standort {
+	
+		protected String Standort;
+		
+		protected Standort(String Standort) {
+			this.Standort=Standort;
+		}
+
+	}
+
+
